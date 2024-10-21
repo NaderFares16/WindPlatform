62dca08e780bfea5437c1238518392dc5cec4ee4
Nader Fares <naderalifares16@hotmail.com>
2024-10-20 22:25:46 -0300

    feat: Return Button

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// CARROSSEL
+
 const images = document.querySelectorAll('.carousel-slide img');
 let currentIndex = 0;
 const transitionTime = 8000;
@@ -18,3 +20,27 @@ function nextImage() {
 
 showImage(currentIndex);
 setInterval(nextImage, transitionTime);
+
+// BOTÃO FLUTUANTE
+
+// Captura o botão
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Função para monitorar o scroll e mostrar/esconder o botão
+window.onscroll = function() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        scrollToTopBtn.classList.add('show');
+        scrollToTopBtn.classList.remove('hide');
+    } else {
+        scrollToTopBtn.classList.add('hide');
+        scrollToTopBtn.classList.remove('show');
+    }
+};
+
+// Função para retornar ao topo suavemente
+scrollToTopBtn.addEventListener("click", function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Animação suave
+    });
+});
